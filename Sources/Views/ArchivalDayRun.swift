fa7c921c263845c5555d7e72a8be81559e84a541
Reed Es <reedes@fastmail.com>
2023-03-10 20:31:58 -0700

    ongoing

--- Sources/Views/ArchivalDayRun.swift
+++ Sources/Views/ArchivalDayRun.swift
@@ -38,3 +38,34 @@ struct ArchivalDayRun: View {
         return Self.df.string(from: dateVal)
     }
 }
+
+struct ArchivalDayRun_Previews: PreviewProvider {
+    static var previews: some View {
+        let manager = CoreDataStack.getPreviewStack()
+        let ctx = manager.container.viewContext
+        let archiveStore = manager.getArchiveStore(ctx)!
+
+        let consumedDay1 = "2023-02-01"
+        let consumedTime1 = "16:05"
+
+        let category1ArchiveID = UUID()
+        let category2ArchiveID = UUID()
+        let serving1ArchiveID = UUID()
+        let serving2ArchiveID = UUID()
+
+        let zc1 = ZCategory.create(ctx, categoryArchiveID: category1ArchiveID, categoryName: "Fruit", toStore: archiveStore)
+        let zc2 = ZCategory.create(ctx, categoryArchiveID: category2ArchiveID, categoryName: "Meat", toStore: archiveStore)
+        let zs1 = ZServing.create(ctx, zCategory: zc1, servingArchiveID: serving1ArchiveID, servingName: "Banana", toStore: archiveStore)
+        let zs2 = ZServing.create(ctx, zCategory: zc2, servingArchiveID: serving2ArchiveID, servingName: "Steak", toStore: archiveStore)
+        let zdr = ZDayRun.create(ctx, consumedDay: consumedDay1, calories: 2433, toStore: archiveStore)
+        _ = ZServingRun.create(ctx, zDayRun: zdr, zServing: zs1, consumedTime: consumedTime1, calories: 120, toStore: archiveStore)
+        _ = ZServingRun.create(ctx, zDayRun: zdr, zServing: zs2, consumedTime: consumedTime1, calories: 450, toStore: archiveStore)
+        try? ctx.save()
+
+        return NavigationStack {
+            ArchivalDayRun(zDayRun: zdr, archiveStore: archiveStore)
+                .environment(\.managedObjectContext, ctx)
+                .environmentObject(manager)
+        }
+    }
+}
