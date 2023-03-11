b83d87d8470514e699573ddb0d51e9b151b0d012
Reed Es <reedes@fastmail.com>
2023-03-10 20:36:42 -0700

    ongoing

--- Sources/Views/HistoryView.swift
+++ Sources/Views/HistoryView.swift
@@ -108,23 +108,41 @@ struct HistoryView: View {
 }
 
 struct HistoryView_Previews: PreviewProvider {
+    struct TestHolder: View {
+        @State var navData: Data?
+        @State var isNew = false
+        var body: some View {
+            DcaltNavStack(navData: $navData) {
+                HistoryView()
+            }
+        }
+    }
+
     static var previews: some View {
         let manager = CoreDataStack.getPreviewStack()
         let ctx = manager.container.viewContext
         let archiveStore = manager.getArchiveStore(ctx)!
 
+        let consumedDay1 = "2023-02-01"
+        let consumedTime1 = "16:05"
+
         let category1ArchiveID = UUID()
         let category2ArchiveID = UUID()
-        let category3ArchiveID = UUID()
+        let serving1ArchiveID = UUID()
+        let serving2ArchiveID = UUID()
 
-        _ = ZCategory.create(ctx, categoryArchiveID: category1ArchiveID, categoryName: "blah", toStore: archiveStore)
-        _ = ZCategory.create(ctx, categoryArchiveID: category2ArchiveID, categoryName: "bleh", toStore: archiveStore)
-        _ = ZCategory.create(ctx, categoryArchiveID: category3ArchiveID, categoryName: "bloop", toStore: archiveStore)
-        // try! ctx.save()
+        let zc1 = ZCategory.create(ctx, categoryArchiveID: category1ArchiveID, categoryName: "Fruit", toStore: archiveStore)
+        let zc2 = ZCategory.create(ctx, categoryArchiveID: category2ArchiveID, categoryName: "Meat", toStore: archiveStore)
+        let zs1 = ZServing.create(ctx, zCategory: zc1, servingArchiveID: serving1ArchiveID, servingName: "Banana", toStore: archiveStore)
+        let zs2 = ZServing.create(ctx, zCategory: zc2, servingArchiveID: serving2ArchiveID, servingName: "Steak", toStore: archiveStore)
+        let zdr = ZDayRun.create(ctx, consumedDay: consumedDay1, calories: 2433, toStore: archiveStore)
+        _ = ZServingRun.create(ctx, zDayRun: zdr, zServing: zs1, consumedTime: consumedTime1, calories: 120, toStore: archiveStore)
+        _ = ZServingRun.create(ctx, zDayRun: zdr, zServing: zs2, consumedTime: consumedTime1, calories: 450, toStore: archiveStore)
+        try? ctx.save()
 
-        return NavigationStack { HistoryView()
-            .environment(\.managedObjectContext, ctx)
-            .environmentObject(manager)
-        }
+        return
+            TestHolder()
+                .environment(\.managedObjectContext, ctx)
+                .environmentObject(manager)
     }
 }
