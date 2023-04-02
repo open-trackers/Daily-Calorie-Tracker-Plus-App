78debae18a265ca6c0b4e5abf3fa095075e33f7b
Reed Es <reedes@fastmail.com>
2023-04-02 14:42:28 -0600

    Fix for confirmationDialog placement being clipped on iPad

--- Sources/Views/HistoryView.swift
+++ Sources/Views/HistoryView.swift
@@ -46,14 +46,16 @@ struct HistoryView: View {
                         }
                     }
                 }
-                .confirmationDialog("",
-                                    isPresented: $showClearDialog,
-                                    actions: {
-                                        Button("Clear", role: .destructive, action: clearHistoryAction)
-                                    },
-                                    message: {
-                                        Text("This will remove all historical data, including today’s.")
-                                    })
+                // NOTE: using an alert, as confirmationDialog may be clipped at top of view on iPad
+                // .confirmationDialog(
+                .alert("",
+                       isPresented: $showClearDialog,
+                       actions: {
+                           Button("Clear", role: .destructive, action: clearHistoryAction)
+                       },
+                       message: {
+                           Text("This will remove all historical data, including today’s.")
+                       })
                 .navigationTitle(navigationTitle)
                 .task(priority: .userInitiated, taskAction)
         } else {
